a91565a2be25bdfc3ee662672abd4cbcdf13e999
bogdan demchenko <bogdandemchenko@gmail.com>
2022-01-26 16:06:04 +0200

    Initial Commit

---
+++ tutorial swift ui/tutorial_swift_uiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tutorial_swift_uiApp.swift
+//  tutorial swift ui
+//
+//  Created by Bogdan Demchenko on 26.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct tutorial_swift_uiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
