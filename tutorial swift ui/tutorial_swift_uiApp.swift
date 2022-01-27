03ea4fbe380550f03d043dff96319c818251b487
bogdan demchenko <bogdandemchenko@gmail.com>
2022-01-27 23:00:53 +0200

    use ModelData object

--- tutorial swift ui/tutorial_swift_uiApp.swift
+++ tutorial swift ui/tutorial_swift_uiApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct tutorial_swift_uiApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
