ff80160134c16d86cf095ce9374ac34422684013
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-02 22:44:34 +0200

    added User Profile

---
+++ tutorial swift ui/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  tutorial swift ui
+//
+//  Created by Bogdan Demchenko on 02.02.2022.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
