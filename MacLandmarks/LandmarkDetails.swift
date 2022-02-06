ce1d1a629af0364d4ef8ba7772697b9b4dcd378d
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-06 18:48:36 +0200

    added macOS detail View

---
+++ MacLandmarks/LandmarkDetails.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetails.swift
+//  MacLandmarks
+//
+//  Created by Bogdan Demchenko on 06.02.2022.
+//
+
+import SwiftUI
+
+struct LandmarkDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetails_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetails()
+    }
+}
