3b8a1fdc4c9d1fdd6e1bb12ac7f692bdb1c0403a
bogdan demchenko <bogdandemchenko@gmail.com>
2022-02-06 18:24:42 +0200

    added macOS target

---
+++ MacLandmarks/MacLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLandmarksApp.swift
+//  MacLandmarks
+//
+//  Created by Bogdan Demchenko on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct MacLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
